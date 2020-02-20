dbb56cc3602c3fe377788a2f0a76e3d396511c89
bw2 <bw2@users.noreply.github.com>
2020-02-20 16:00:11 -0500

    Create export_tsvs.py
    This hail script takes the hail table generated by Mike's script and outputs one .tsv for each sample which contains all the variants that will be used from that sample.

---
+++ export_tsvs.py
@@ -0,0 +1,48 @@
+import argparse
+import hail as hl
+import os
+
+p = argparse.ArgumentParser()
+p.add_argument("-o", "--output-bucket-path", help="Path where the .tsvs for each sample will be written.", default="gs://gnomad-bw2/gnomad_readviz_tsvs")
+p.add_argument("sample_ids_file_path", help="A text file containing one sample id per line")
+args = p.parse_args()
+
+sample_ids = []
+with open(args.sample_ids_file_path, "rt") as f:
+    for line in f:
+        sample_ids.append(line.rstrip("\n"))
+
+
+ht = hl.read_table("gs://gnomad/readviz/genomes_v3/gnomad_v3_readviz_crams.ht")
+
+# add het_or_hom_or_hemi field to each struct
+ht = ht.annotate(
+    samples_w_het_var=ht.samples_w_het_var.map(lambda x: hl.struct(S=x.S, GQ=x.GQ, het_or_hom_or_hemi=1)),
+    samples_w_hom_var=ht.samples_w_hom_var.map(lambda x: hl.struct(S=x.S, GQ=x.GQ, het_or_hom_or_hemi=2)),
+    samples_w_hemi_var=ht.samples_w_hemi_var.map(lambda x: hl.struct(S=x.S, GQ=x.GQ, het_or_hom_or_hemi=3))
+)
+
+# combine ht.samples_w_het_var, ht.samples_w_hom_var, ht.samples_w_hemi_var into a single ht.samples array
+ht = ht.annotate(samples=ht.samples_w_het_var.extend(ht.samples_w_hom_var.extend(ht.samples_w_hemi_var)))
+ht = ht.drop(ht.samples_w_het_var, ht.samples_w_hom_var, ht.samples_w_hemi_var)
+
+# explode
+ht = ht.explode(ht.samples)
+
+
+# write a .tsv for each sample
+for s in sample_ids:
+    print(f"Processing sample: {s}")
+    ht = ht.filter(ht.samples.S==s, keep=True)
+    ht = ht.key_by()
+    ht = ht.transmute(
+        chrom=ht.locus.contig,
+        pos=ht.locus.position,
+        ref=ht.alleles[0],
+        alt=ht.alleles[0],
+        GQ=ht.samples.GQ,
+        het_or_hom_or_hemi=ht.samples.het_or_hom_or_hemi,
+    )
+
+    s = s.replace(' ', '__').replace(":", "_")
+    ht.export(os.path.join(args.sample_ids_file_path, f"{s}.tsv.bgz"), header=True)
