4dad808e4f7f404da5b53280c5af91a9d2c70c5d
bw2 <bw2@users.noreply.github.com>
2020-02-23 15:10:01 -0500

    bash script for running export_tsvs.py on many small dataproc clusters
    export_tsvs.py takes 15 seconds per sample id (once the input table is reshuffled and keyed by sample). 
    This running time doesn't improve with cluster size because the ht has ~50,000 partitions so on average all data for one sample sits on a single partition. This means all samples can be processed in ~10 hours on 20 small dataproc clusters which have 2 worker nodes each. This script creates these 20 clusters, submits jobs, and later deletes the clusters.
    
    It may be possible to improve per-sample runtime by first repartitioning the table to ~500k partitions, using clusters with more worker nodes, and exporting sharded tsvs instead of a single .tsv.bgz. But these .tsvs would be more difficult to handle in the downstream step.

---
+++ run_export_tsvs.sh
@@ -0,0 +1,37 @@
+# path of file containing all sample ids
+SAMPLE_IDS_PATH=$1
+if [ -f "${SAMPLE_IDS_PATH}" ]; then
+    TOTAL_SAMPLE_IDS=$(cat ${SAMPLE_IDS_PATH} | wc -l)
+else
+    TOTAL_SAMPLE_IDS=$(gsutil cat ${SAMPLE_IDS_PATH} | wc -l)
+fi
+
+echo "Found ${TOTAL_SAMPLE_IDS} sample ids in: ${SAMPLE_IDS_PATH}"
+
+# number of sample ids to process per dataproc cluster
+N_SAMPLES_PER_CLUSTER=3432  # since each sample id takes ~15 seconds, each cluster should take cluster ~14.5 hours, and 20 clusters are needed to process all 68339 sample ids
+
+# hailctl dataproc args
+N_WORKERS=2
+N_PREEMPTIBLES=0
+WORKER_MACHINE_TYPE="n1-standard-1"
+MASTER_MACHINE_TYPE="n1-standard-4"
+PROJECT=broad-mpg-gnomad
+PACKAGES_ARG="slackclient==2.0.0,websocket-client,sklearn,statsmodels,scikit-learn,hdbscan"
+INIT_ARG="gs://gnomad-public/tools/inits/master-init.sh"
+
+for i in $(seq 0 ${N_SAMPLES_PER_CLUSTER} ${TOTAL_SAMPLE_IDS} )
+do
+    let k=${i}/${N_SAMPLES_PER_CLUSTER}
+    CLUSTER_NAME="readviz-tsv-cluster${k}"
+
+    echo "Starting ${CLUSTER_NAME}. Log file: ${CLUSTER_NAME}.log"
+    set -x
+    (
+	hailctl dataproc start --num-workers ${N_WORKERS} --num-preemptible ${N_PREEMPTIBLES} --max-idle 10m --project ${PROJECT} --init ${INIT_ARG} --packages ${PACKAGES_ARG} ${CLUSTER_NAME} \
+	    --master-machine-type ${MASTER_MACHINE_TYPE} --worker-machine-type ${WORKER_MACHINE_TYPE} \
+    	&& hailctl dataproc submit ${CLUSTER_NAME} export_tsvs.py --start-with-sample-i ${i} --n-samples-to-process ${N_SAMPLES_PER_CLUSTER} ${SAMPLE_IDS_PATH} \
+    	&& gcloud --project ${PROJECT} dataproc clusters delete --quiet ${CLUSTER_NAME} \
+    ) >& ${CLUSTER_NAME}.log &
+    set +x
+done
