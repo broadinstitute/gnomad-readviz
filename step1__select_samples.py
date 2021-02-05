056e9553a0945df8d8c40c602ba80cbc740509b7
bw2 <weisburd@broadinistitute.org>
2021-02-05 12:13:10 -0500

    default ht paths

--- step1__select_samples.py
+++ step1__select_samples.py
@@ -132,8 +132,9 @@ if __name__ == "__main__":
     parser.add_argument(
         "--output-ht-path",
         help="Path for output hail table",
-        default="gs://gnomad-bw2/gnomad_v3_readviz_crams__that_failed_AB_filter.ht",
-        #default="gs://gnomad-bw2/gnomad_v3_1_readviz_crams__that_failed_AB_filter.ht",
+        default="gs://gnomad-bw2/gnomad_v3_1_readviz_crams.ht",
+        #default="gs://gnomad/readviz/genomes_v3/gnomad_v3_readviz_crams.ht",
+        #
     )
     args = parser.parse_args()
 
