b33cb6b4386c03d4c610f2545b0c190442f63d00
bw2 <weisburd@broadinistitute.org>
2021-02-25 11:50:11 -0500

    example step1 command-line

---
+++ run_step1__select_samples.sh
@@ -0,0 +1,17 @@
+set -x
+
+hailctl dataproc start \
+	--num-workers 2 \
+	--num-preemptible 0 \
+	--region us-central1 \
+	--autoscaling-policy=dataproc-autoscale \
+	--pkgs luigi,google-api-python-client,gnomad \
+	--max-idle 8h \
+	readviz-select-samples
+
+hailctl dataproc submit \
+	--pyfiles="/Users/weisburd/code/gnomad_qc/gnomad_qc,/Users/weisburd/code/gnomad_methods/gnomad" \
+	readviz-select-samples \
+	step1__select_samples.py \
+	--meta-table-ht gs://gnomad/metadata/genomes_v3.1/gnomad_v3.1_sample_qc_metadata.ht \
+	--output-ht-path gs://gnomad-readviz/v3_and_v3.1/gnomad_v3_1_readviz_crams.ht
