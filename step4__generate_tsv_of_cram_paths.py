f466b746e1010959fc60654d8eb41cbe377bbdb2
Katherine Chao <kchao@broadinstitute.org>
2021-08-03 11:54:25 -0400

    Updated step4 to be a submit script that uses parallel_file_exists

--- step4__generate_tsv_of_cram_paths.py
+++ step4__generate_tsv_of_cram_paths.py
@@ -1,71 +1,81 @@
-#%%
-import datetime
-import pandas as pd
-import re
+import asyncio
+import argparse
+import logging
+
 import hail as hl
-import os
-#%%
 
-"""
-ht_meta = hl.read_table("gs://gnomad/metadata/genomes_v3.1/gnomad_v3.1_sample_qc_metadata.ht")
-v31_all_samples = ht_meta.s.collect()      # 153,030 samples
-ht_meta =  ht_meta.filter(ht_meta.release)
-v31_release_samples = ht_meta.s.collect()  # 76,156 samples
+from gnomad.resources.resource_utils import DataException
+from gnomad.utils.file_utils import parallel_file_exists
 
+from .utils import get_sample_ids
 
-ht = hl.read_table("gs://gnomad-bw2/gnomad_v3_1_readviz_crams__that_failed_AB_filter_exploded_keyed_by_sample.ht")
-current_samples_v31 = ht.distinct().S.collect()  # 4,445 samples
-len(set(current_samples_v31) - set(v31_release_samples))   # 919 samples  
-
-ht = hl.read_table("gs://gnomad-bw2/gnomad_v3_readviz_crams__that_failed_AB_filter_exploded_keyed_by_sample.ht")
-current_samples_v3 = ht.distinct().S.collect()  # 68,639 samples
-
-len(set(current_samples_v3) - set(v31_release_samples)) # 1,655
-
-"""
-
-#%%
-
-tsv_paths = hl.hadoop_ls("gs://gnomad-bw2/gnomad_v3_1_readviz_tsvs")
-
-#%%
-
-path_tuples = [(os.path.basename(t['path']).replace(".tsv.bgz", ""), t['path']) for t in tsv_paths]
-df = pd.DataFrame(path_tuples, columns=['entity:participant_id', 'variants_tsv_bgz'])
-df = df.set_index('entity:participant_id')
-
-#%%
-
-df2 = pd.read_table("./metadata/v3_1_new_releasable_cram_paths_with_sex.txt").rename(columns={'CRAM': 'cram_path', 'CRAI': 'crai_path'})
-df2 = df2[['sample_id', 'cram_path', 'crai_path']]
-df2 = df2.set_index('sample_id')
-
-#%%
-
-df_final = df.join(df2, how="inner")
-assert sum(df_final.variants_tsv_bgz.isna()) == 0
-assert sum(df_final.cram_path.isna()) == 0
-
-df_final.columns
-
-#%%
-
-df_final.reset_index().rename(
-    columns={'index': 'sample_id'}).to_csv(
-    "step4_output__cram_and_tsv_paths_table.tsv", index=False, header=True, sep="\t")
+logging.basicConfig(format="%(levelname)s (%(name)s %(lineno)s): %(message)s")
+logger = logging.getLogger("get_tsv_for_haplotype_caller")
+logger.setLevel(logging.INFO)
 
 
-#%%
+def main(args):
+    """
+    Generate single TSV with sample IDs, cram paths, and variant TSV file paths.
 
-#%%
-df_final = df_final.reset_index().rename(columns={'index': 'entity:participant_id'})
-df_final.to_csv("step4_output__terra_tsv_paricipants.tsv", index=False, header=True, sep="\t")
+    NOTE: Run locally (`parallel_file_exists` only works locally).
+    """
+    output_bucket = args.output_bucket
 
-#%%
+    logger.info("Getting sample IDs...")
+    sample_ids = get_sample_ids(args.ids_file)
 
-df_final['membership:participant_set_id'] = "sample_set_" + datetime.datetime.now().strftime("%Y_%m_%d")
-df_final['participant'] = df_final["entity:participant_id"]
-df_final = df_final[['membership:participant_set_id', "participant"]]
-df_final.to_csv("step4_output__terra_tsv_participant_set.tsv", index=False, header=True, sep="\t")
+    tsvs = [f"{output_bucket}/{sample}.tsv.bgz" for sample in sample_ids]
+    tsv_file_exists = asyncio.get_event_loop().run_until_complete(
+        parallel_file_exists(tsvs)
+    )
 
-#%%
+    logger.info("Starting cram existence checks...")
+    cram_map = {}
+    with hl.hadoop_open(args.cram_map) as c:
+        for line in c:
+            sample, cram = line.strip().split("\t")
+            cram_map[sample] = cram
+    crams_exist = asyncio.get_event_loop().run_until_complete(
+        parallel_file_exists(list(cram_map.values()))
+    )
+
+    logger.info("Starting to write to output TSV...")
+    with hl.hadoop_open(args.cram_map) as s, hl.hadoop_open(
+        f"{output_bucket}/inputs/step4_output_cram_and_tsv_paths_table.tsv", "w",
+    ) as o:
+        o.write("sample_id\tcram\tcrai\tvariants_tsv_bgz\n")
+
+        for line in s:
+            sample = line.strip()
+            if sample not in cram_map:
+                raise DataException(
+                    f"{sample} is missing a cram path. Please double check and restart!"
+                )
+            else:
+                cram = cram_map[sample]
+                tsv = f"{output_bucket}/{sample}.tsv.bgz"
+                if not crams_exist[cram]:
+                    raise DataException(
+                        f"{sample}'s cram does not exist. Please double check and restart!"
+                    )
+            if not tsv_file_exists[tsv]:
+                raise DataException(
+                    f"{sample} is missing their variants TSV file. Please double check and restart!"
+                )
+            o.write(f"{sample}\t{cram}\t{cram}.crai\t{tsv}\n")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ids-file", help="Path to local file with sample IDs.")
+    parser.add_argument(
+        "--cram-map",
+        help="Path to file in cloud storage with cram to sample mapping. File should have two columns: sample ID and cram path.",
+    )
+    parser.add_argument(
+        "--output-bucket", help="Path to output bucket (where to store output TSVs)."
+    )
+    args = parser.parse_args()
+
+    main(args)
