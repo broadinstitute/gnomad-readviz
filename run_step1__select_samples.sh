cb581bafb78128384f1f906d480f113c7a9e7e1f
bw2 <ben.weisburd@gmail.com>
2023-05-24 21:31:53 -0400

    updated commands to work with the latest version of hail and gnomAD v4

--- run_step1__select_samples.sh
+++ run_step1__select_samples.sh
@@ -1,16 +1,18 @@
 set -x
 
+cluster_name=readviz-select-samples
+
 hailctl dataproc start \
+	--requester-pays-allow-all \
 	--num-workers 2 \
 	--num-preemptible 0 \
 	--region us-central1 \
 	--autoscaling-policy=dataproc-autoscale \
-	--pkgs luigi,google-api-python-client,gnomad \
-	--max-idle 8h \
-	readviz-select-samples
+	--pkgs luigi,google-api-python-client,gnomad,"git+https://github.com/broadinstitute/gnomad_methods.git@main","git+https://github.com/broadinstitute/gnomad_qc.git@main" \
+	--max-idle 3h \
+	--worker-machine-type n1-highmem-8 \
+	$cluster_name
 
-hailctl dataproc submit \
-	readviz-select-samples \
-	step1__select_samples.py \
-	--meta-table-ht gs://gnomad/metadata/genomes_v3.1/gnomad_v3.1_sample_qc_metadata.ht \
-	--output-ht-path gs://gnomad-readviz/v3_and_v3.1/gnomad_v3_1_readviz_crams.ht
+hailctl dataproc submit  $cluster_name step1__select_samples.py  --overwrite \
+  --sample-metadata-tsv gs://gnomad-readviz/v4.0/gnomad.exomes.v4.0.metadata.tsv.gz \
+  --output-ht-path gs://gnomad-readviz/v4.0/gnomad.exomes.v4.0.readviz_crams.ht
